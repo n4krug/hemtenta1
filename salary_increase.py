41daa9d9895e1d98c06d14823685b31620c3285e
n4krug <gustav.eneberg@gmail.com>
2021-10-24 19:45:44 +0200

    commented code

--- salary_increase.py
+++ salary_increase.py
@@ -15,34 +15,48 @@ def main():
     # Innitial layout for employe salary column:
     next_year_column = [[sg.Text(text="Lön nästa år (1 månad):")]]
 
+    # Create initial layout
     layout = create_layout(
         name_column, salary_column, next_year_column, None, None, "5"
     )
 
+    # Create main window
     window = sg.Window("Löneöknings räknare", layout=layout, margins=(100, 50))
 
+    # create a base dict (used for GUI before calculations)
     next_year_salaries = {
         "total": "",
         "employes": ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
     }
 
+    # Create a default value for procentual increase
     increase = "5"
 
+    # Event Loop
     while True:
+
+        # Read events from GUI window
         event, values = window.read()
 
+        # Close event
         if event == sg.WINDOW_CLOSED:
             break
 
         # Import button (run function to import dict from file through GUI)
         elif event == "Importera Löner":
+            # Function imported from salary_calculator.py
             salaries = import_salaries("salaries-2019.json")
 
+        # calculate next year's salaries event
         elif event == "Räkna ut nästa års löner":
+
+            # update the dict with values from GUI inputs (function imported from salary_calculator.py)
             salaries = update_dict(values, salaries)
 
+            # Set procentual increase value to GUI input
             increase = values["-SALARY_INCREASE-"]
 
+            # calculate next years salaries
             next_year_salaries = calc_next_year(salaries, increase)
 
         # Create lists from dict (helps for printing to GUI)
@@ -69,6 +83,7 @@ def main():
 
         # update salaries list on GUI
         for i in range(len(salaries_list)):
+            # Update GUI text for salaries based on dict
             salary_column.append(
                 [
                     sg.Input(
@@ -78,6 +93,7 @@ def main():
                 ]
             )
 
+            # Update GUI text for next years salaries based on dict
             next_year_column.append(
                 [
                     sg.Text(
@@ -101,9 +117,11 @@ def main():
         window.close()
         window = window1
 
+    # Close window
     window.close()
 
 
+# Create a layout function
 def create_layout(
     name_column,
     salary_column,
@@ -112,6 +130,7 @@ def create_layout(
     next_year_total,
     increase,
 ):
+    # create the layout
     layout = [
         # top buttons:
         [sg.Button("Importera Löner")],
@@ -150,21 +169,28 @@ def create_layout(
         ],
     ]
 
+    # return the layout
     return layout
 
 
+# function for calculating next years salaries
 def calc_next_year(salaries, increase):
+
+    # Create a default dict
     next_year_salaries = {
         "total": 0,
         "employes": [],
     }
 
+    # convert increase from percent to a factor
     factorial_increase = 1 + int(increase) / 100
 
+    # Calculate next year's total salary for entire company
     next_year_salaries["total"] = int(
         int(salaries["company-yearly"]) * factorial_increase
     )
 
+    # Loop through salaries and calculate next years salary
     for key in salaries["salaries-monthly"]:
         next_year_salary = int(
             int(salaries["salaries-monthly"][key]) * factorial_increase
@@ -172,8 +198,7 @@ def calc_next_year(salaries, increase):
 
         next_year_salaries["employes"].append(next_year_salary)
 
-    print(next_year_salaries)
-
+    # return the dict
     return next_year_salaries
 
 
